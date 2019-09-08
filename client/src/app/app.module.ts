ccbdeadca5859bfcff1ff38945ecc7f0475eea82
Philippe Turner <philippeturner@gmail.com>
2019-09-08 12:04:10 -0400

    [wip] - Modification pour ajout du material design

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModules } from './app.material-modules';
 import { AppComponent } from './components/app/app.component';
 import { NewDrawingComponent } from './components/new-drawing/new-drawing.component';
+import { ColorPickerComponent } from './components/color-picker/color-picker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewDrawingComponent,
+    ColorPickerComponent,
   ],
   imports: [
     BrowserModule,
