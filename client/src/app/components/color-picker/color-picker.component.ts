b2fb3443064d5348204ded86780cd585b90d6cf4
Philippe Turner <philippeturner@gmail.com>
2019-09-09 12:06:49 -0400

    [wip] - Ajout de color picker dans la fenetre modal

--- client/src/app/components/color-picker/color-picker.component.ts
+++ client/src/app/components/color-picker/color-picker.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-color-picker',
   templateUrl: './color-picker.component.html',
-  styleUrls: ['./color-picker.component.scss']
+  styleUrls: ['./color-picker.component.scss'],
 })
-export class ColorPickerComponent implements OnInit {
+export class ColorPickerComponent {
 
-  @Input() color: number;
-  constructor() { }
-
-  ngOnInit() {
-  }
+  hue: { r: number, g: number, b: number };
+  rgb: { r: number, g: number, b: number } = { r: 255, g: 255, b: 255 };
+  a = 1;
+  color: string;
 
 }
