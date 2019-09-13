0c13d3bdf2724f9824c7f6ba3b095f97902afdaf
Kevin Nguyen <xuannamkevin@gmail.com>
2019-09-13 15:59:45 -0400

    [wip] servir du texte sur index/text

--- server/app/services/index.service.ts
+++ server/app/services/index.service.ts
@@ -3,20 +3,31 @@ import 'reflect-metadata';
 import {Message} from '../../../common/communication/message';
 import Types from '../types';
 import {DateService} from './date.service';
+import * as fs from 'fs';
 
 @injectable()
 export class IndexService {
+
     constructor(
         @inject(Types.DateService) private dateService: DateService,
     ) {
     }
 
     about(): Message {
+        
         return {
             title: 'This is merely a test',
             body: 'Lorem ipsum........',
         };
     }
+
+     getTextRessource():JSON {
+        const file =fs.readFileSync("/Users/kevin/Documents/LOG2990/PolyDessin-E16/server/app/services/res/text/welcome_text2.json");
+        let obj = JSON.parse(file.toString());
+        // res.json(obj)
+        return obj;
+    
+        }
 
     async helloWorld(): Promise<Message> {
         return this.dateService.currentTime().then((timeMessage: Message) => {
