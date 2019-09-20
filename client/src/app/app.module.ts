7c6c7630e7c44f9ce7d3febd08a38719a7ca10f0
Philippe Turner <philippeturner@gmail.com>
2019-09-20 10:33:29 -0400

    Correction erreur de merge

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MaterialModules } from './app.material-modules';
 import { ColorPickerModule } from './color-picker/color-picker.module';
 import { AppComponent } from './components/app/app.component';
-<<<<<<< HEAD
 import { NewDrawingAlertComponent } from './components/new-drawing/new-drawing-alert/new-drawing-alert.component';
 import { NewDrawingFormComponent } from './components/new-drawing/new-drawing-form/new-drawing-form.component';
 import { NewDrawingComponent } from './components/new-drawing/new-drawing.component';
 
-=======
->>>>>>> master
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,11 +25,8 @@ import { NewDrawingComponent } from './components/new-drawing/new-drawing.compon
     HttpClientModule,
     ReactiveFormsModule,
     MaterialModules,
-<<<<<<< HEAD
     ColorPickerModule,
-=======
     FontAwesomeModule,
->>>>>>> master
   ],
   exports: [
   ],
