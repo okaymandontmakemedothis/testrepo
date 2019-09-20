b415e71e3bcb7e4e450b3d892c99136942abe056
Philippe Turner <philippeturner@gmail.com>
2019-09-20 10:28:20 -0400

    [done] - Le color picker est fonctionnel

--- client/src/app/color-picker/color-picker.service.spec.ts
+++ client/src/app/color-picker/color-picker.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-
+import { FormControl, FormGroup } from '@angular/forms';
 import { ColorPickerService } from './color-picker.service';
 
 describe('ColorPickerService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: ColorPickerService;
+  beforeEach(() => {
+    service = TestBed.get(ColorPickerService);
+    TestBed.configureTestingModule({});
+  });
 
   it('should be created', () => {
-    const service: ColorPickerService = TestBed.get(ColorPickerService);
     expect(service).toBeTruthy();
+  });
+
+  it('#rgb should return rgb group', () => {
+    const rgb: FormGroup = service.rgb;
+    expect((rgb.get('r') as FormControl).value).toBe(255);
+    expect((rgb.get('g') as FormControl).value).toBe(255);
+    expect((rgb.get('b') as FormControl).value).toBe(255);
   });
 });
