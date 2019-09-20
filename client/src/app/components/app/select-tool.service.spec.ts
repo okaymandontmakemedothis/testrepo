e15294956dc33a343976e23db489766f12dcf6b4
Serge GNAVO <gnavoserge@gmail.com>
2019-09-20 10:52:17 -0400

    ajout des details pinceaux et des detail dessins

--- client/src/app/components/app/select-tool.service.spec.ts
+++ client/src/app/components/app/select-tool.service.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { SelectToolService } from './select-tool.service';
 
 describe('Service: SelectTool', () => {
--- client/src/app/components/app/select-tool.service.ts
+++ client/src/app/components/app/select-tool.service.ts
@@ -1,16 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable ,Input } from '@angular/core';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 @Injectable({
   providedIn: 'root'
 })
 export class SelectToolService {
-private currentIcon: IconDefinition ;
-constructor() { }
+@ Input()  currentIcon: IconDefinition ;
+constructor() {
+
+
+ }
 
 
   setIcone(icon: IconDefinition) {
 
     this.currentIcon = icon;
-    console.log('reussi');
+    console.log('this.currentIcon');
   }
 }
