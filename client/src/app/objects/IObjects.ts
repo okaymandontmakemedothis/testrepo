64adb1aecf71e7a4785de74ac24fcfd7b090a8dc
Philippe Turner <philippeturner@gmail.com>
2019-09-23 15:17:18 -0400

    [wip] - Modification interfaces

--- client/src/app/objects/IObjects.ts
+++ client/src/app/objects/IObjects.ts
@@ -1,6 +1,10 @@
 export interface IObjects {
-    id:number;
-    svgLine:string;
-    draw($event:MouseEvent):string;
-    getColor():void;
+    id: number;
+    x: number
+    y: number
+    height: number
+    width: number
+    draw($event: MouseEvent): string;
+    primaryColor: RGBA;
+    secondaryColor: RGBA;
 }
