c90ffac0837cfab634007c063bf9be9bb092e72a
Philippe Turner <philippeturner@gmail.com>
2019-09-23 15:14:57 -0400

    [wip] - Test tools

--- client/src/app/components/app/app.component.spec.ts
+++ client/src/app/components/app/app.component.spec.ts
@@ -1,16 +1,11 @@
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import {RouterTestingModule} from '@angular/router/testing';
-import {of} from 'rxjs';
-import {IndexService} from '../../services/index/index.service';
-import SpyObj = jasmine.SpyObj;
 import { WelcomeDialogModule } from '../welcome-dialog/welcome-dialog.module';
 import { MaterialModules } from './../../app.material-modules';
-import {AppComponent} from './app.component';
-
-import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialog } from '@angular/material';
 
