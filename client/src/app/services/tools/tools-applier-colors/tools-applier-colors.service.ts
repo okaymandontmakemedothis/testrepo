27bf287c04d2bdbf5cc68dc878292a89a4a1f37e
Kevin Nguyen <xuannamkevin@gmail.com>
2019-09-23 17:22:03 -0400

    [done] merge rect tools en progres

--- client/src/app/services/tools/tools-applier-colors/tools-applier-colors.service.ts
+++ client/src/app/services/tools/tools-applier-colors/tools-applier-colors.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { ITools } from '../ITools';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class ToolsApplierColorsService {
-
+export class ToolsApplierColorsService implements ITools {
+  // id: number;
+  // onPressed($event: MouseEvent): IObjects;
+  // onRelease($event: MouseEvent): void;
+  // onMove($event: MouseEvent): void;
+  id:number =2;
+  name:string = "Applier"l
+  onPressed($event:MouseEvent):string{
+    return ""
+  }
+  
   constructor() { }
 }
