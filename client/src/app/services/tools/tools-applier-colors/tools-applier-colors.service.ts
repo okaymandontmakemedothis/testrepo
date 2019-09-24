df2a157b31d7b1d84ef12ffacaa55a46b33add8b
Sophie <s.dorval06@gmail.com>
2019-09-24 12:45:32 -0400

    [wip] change de couleur au click mais ne reste pas quand on release le click

--- client/src/app/services/tools/tools-applier-colors/tools-applier-colors.service.ts
+++ client/src/app/services/tools/tools-applier-colors/tools-applier-colors.service.ts
@@ -4,6 +4,9 @@ import { RGBA } from '../../../model/rgba.model';
 import { ITools } from '../ITools';
 import { ColorRgbaHexComponent } from 'src/app/color-picker/color-rgba-hex/color-rgba-hex.component';
 import { isObject } from 'util';
+import { TargetLocator } from 'selenium-webdriver';
+import { RectangleObject } from 'src/app/objects/object-rectangle/rectangle';
+import { TemplateBindingParseResult } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root',
@@ -15,26 +18,27 @@ export class ToolsApplierColorsService implements ITools {
   // onMove($event: MouseEvent): void;
   id = 2;
   name = 'Applier';
+  object: IObjects;
   onPressed($event: MouseEvent): string {
       if ($event.button === 0) { // left click so set fill to a color
         console.log($event.target);
         const target = $event.target as Element;
-        target.setAttribute('width', '150');
+        target.setAttribute('style', 'fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,255)');
         console.log(target);
+
       } else {     // right click so set stroke to a color
         document.addEventListener('contextmenu', ($event2) => {
           $event2.preventDefault(); // prevents the context menu of a right click to show
         });
         console.log($event.target);
       }
-    
-    return 'objet';
+      return this.object.draw($event);
   }
   onRelease($event: MouseEvent): string {
-    return '';
+    return this.object.draw($event);
   }
   onMove($event: MouseEvent): string {
-    return '';
+    return this.object.draw($event);
   }
 
  
