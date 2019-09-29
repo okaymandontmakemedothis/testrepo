0866da5e71de29da7501ac00093189cd7401a64b
Sophie <s.dorval06@gmail.com>
2019-09-29 18:57:30 -0400

    merge of master

--- client/src/app/components/welcome-dialog/aide-dialog/aide-dialog.component.ts
+++ client/src/app/components/welcome-dialog/aide-dialog/aide-dialog.component.ts
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-import { HttpClient } from '@angular/common/http';
-import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
-=======
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
->>>>>>> 353a1150539931da41f2743cdf6d776ebbee2cbb
 import { BehaviorSubject } from 'rxjs';
 import { ShortcutClavier } from '../../../../../../common/communication/message';
 import { IndexService } from '../../../services/index/index.service';
@@ -22,14 +16,6 @@ export class AideDialogComponent {
     Sup: '', A: '', Z: '', ShiftZ: '', Cray: '', W: '', P: '', Y: '', Aer: '', Rec: '', Ell: '', Poly: '',
     L: '', T: '', R: '', B: '', Eff: '', I: '', Sel: '', Gri: '', M: '', Aug: '', Dim: ''
   });
-<<<<<<< HEAD
-  constructor(
-    public dialogRef: MatDialogRef<AideDialogComponent>, private basicService: IndexService, ) {
-    this.basicService.aideGet()
-      .subscribe(this.messageA);
-  }
-
-=======
 
   constructor(
     public dialogRef: MatDialogRef<AideDialogComponent>, private basicService: IndexService, ) {
@@ -38,7 +24,6 @@ export class AideDialogComponent {
       .subscribe(this.messageA);
   }
   // fonction close qui permet de fermer le mat dialog d'aide
->>>>>>> 353a1150539931da41f2743cdf6d776ebbee2cbb
   close(): void {
     this.dialogRef.close();
   }
