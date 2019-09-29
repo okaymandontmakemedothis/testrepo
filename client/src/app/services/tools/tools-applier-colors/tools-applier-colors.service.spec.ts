977f0d47be59ddeb64b6369e4d296985df2bde98
Sophie <s.dorval06@gmail.com>
2019-09-29 15:45:00 -0400

    added mock object class

--- client/src/app/services/tools/tools-applier-colors/tools-applier-colors.service.spec.ts
+++ client/src/app/services/tools/tools-applier-colors/tools-applier-colors.service.spec.ts
@@ -1,6 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ToolsApplierColorsService } from './tools-applier-colors.service';
+import { DrawingService } from '../../drawing/drawing.service';
+import { IObjects } from 'src/app/objects/IObjects';
+import { RGBA } from 'src/app/model/rgba.model';
+
+class MockOject implements IObjects{
+  id: number;
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  primaryColor: RGBA;
+  secondaryColor: RGBA;
+  draw(): string {
+    return '';
+  }
+}
 
 describe('ToolsApplierColorsService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -10,9 +26,19 @@ describe('ToolsApplierColorsService', () => {
     expect(service).toBeTruthy();
   });
 
+  it('should get object from id', () => {
+    const mockDrawingService: DrawingService = new DrawingService();
+    const obj: IObjects = ;
+    const eventMouseDown = new MouseEvent('click');
+    const object = mockDrawingService.getObject(obj.id);
+    expect(obj).toEqual(object);
+  });
+
   it('should change primary color of object on left mouse press', () => {
     const service: ToolsApplierColorsService = TestBed.get(ToolsApplierColorsService);
     const eventMouseDown = new MouseEvent('click');
-     
+    if()
+    service.onPressed(eventMouseDown);
+
   });
 });
