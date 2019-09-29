e2489d765536a32a529a915ea92268a382e23b00
Sophie <s.dorval06@gmail.com>
2019-09-29 18:04:34 -0400

    fix some test from welcome dialog files

--- client/src/test.ts
+++ client/src/test.ts
@@ -1,11 +1,14 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
-
+import 'zone.js/dist/zone-testing';
+/* Disable puisque lorsque certain test sont rouler sur jasmine une erreur :'TypeError: Cannot read
+property 'assertPresent' of undefined:' s'affiche et l'une des solution trouver sur github etait de
+mettre le zone.js au debut : 'The zone import must come before all other imports.' */
+// tslint:disable-next-line: ordered-imports
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-import 'zone.js/dist/zone-testing';
 
 // tslint:disable-next-line:no-any from cli
 declare const require: any;
@@ -17,6 +20,6 @@ getTestBed().initTestEnvironment(
 );
 // Then we find all the tests.
 // const context = require.context('./', true, /\.spec\.ts$/);
-const context = require.context('./', true, /tools-applier-colors.service.spec\.ts$/);
+const context = require.context('./', true, /dialog.component.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
