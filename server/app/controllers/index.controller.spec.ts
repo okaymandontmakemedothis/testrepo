75ecbf835d0fe4e28f2465c4f6c33e49e3790bd8
Kevin Nguyen <xuannamkevin@gmail.com>
2019-10-01 00:49:37 -0400

    [done] setup chai tryout

--- server/app/controllers/index.controller.spec.ts
+++ server/app/controllers/index.controller.spec.ts
@@ -1,6 +1,12 @@
 import {assert} from 'chai';
 
+let chaiHttp = require('chai-http');
+
+
+//Configure chai
+chai.use(chaiHttp)
 it('Sample test: should complete this test', (done) => {
     assert.ok(true);
     done();
 });
+
