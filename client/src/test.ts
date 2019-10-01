6b3179f132f3792b2ccef6e62ade9dfbeb12cad0
Philippe Turner <philippeturner@gmail.com>
2019-10-01 11:04:30 -0400

    [done] - tous les tests des services passent

--- client/src/test.ts
+++ client/src/test.ts
@@ -19,7 +19,7 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context("./", true, /open-welcome.service.spec\.ts$/);
+const context = require.context("./", true, /.spec\.ts$/);
 // const context = require.context('./', true, /tools-color.service.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
