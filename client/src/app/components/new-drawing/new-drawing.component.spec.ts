93cc6a1f2d5dfae02f679c3833ea250d6425ad2d
Philippe Turner <philippeturner@gmail.com>
2019-10-01 11:44:42 -0400

    [wip] - new drawing en train de fix

--- client/src/app/components/new-drawing/new-drawing.component.spec.ts
+++ client/src/app/components/new-drawing/new-drawing.component.spec.ts
@@ -2,13 +2,14 @@ import { Overlay } from '@angular/cdk/overlay';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MAT_DIALOG_DATA, MAT_DIALOG_SCROLL_STRATEGY, MatDialog, MatDialogRef, MatSnackBar } from '@angular/material';
+import { MAT_DIALOG_DATA, MAT_DIALOG_SCROLL_STRATEGY, MatDialog, MatDialogRef, MatSnackBar, MatDialogModule } from '@angular/material';
 import { ColorPickerService } from 'src/app/color-picker/color-picker.service';
 import { ColorTransformerService } from 'src/app/services/color-transformer/color-transformer.service';
 import { DrawingService } from 'src/app/services/drawing/drawing.service';
 import { NewDrawingService } from 'src/app/services/new-drawing/new-drawing.service';
 import { WorkspaceService } from 'src/app/services/workspace/workspace.service';
 import { NewDrawingComponent } from './new-drawing.component';
+import { of } from 'rxjs';
 
 describe('NewDrawingComponent', () => {
   let component: NewDrawingComponent;
@@ -19,22 +20,28 @@ describe('NewDrawingComponent', () => {
   const drawingService: DrawingService = new DrawingService();
   const colorTransformerService: ColorTransformerService = new ColorTransformerService();
   const colorPickerService: ColorPickerService = new ColorPickerService(colorTransformerService);
+  // let dialogSpy: jasmine.Spy;
+  const dialogRefSpyObj = jasmine.createSpyObj({
+    afterClosed: of({}),
+    close: null,
+  });
+  dialogRefSpyObj.componentInstance = { body: "" };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, FormsModule],
+      imports: [MatDialogModule,ReactiveFormsModule, FormsModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
       declarations: [NewDrawingComponent],
       providers: [Overlay, MatSnackBar,
-        { provide: MatDialog, useValue: [] },
         { provide: MAT_DIALOG_SCROLL_STRATEGY, useValue: [] },
         { provide: MatDialogRef, useValue: ['afterOpened'] },
         { provide: MAT_DIALOG_DATA, useValue: [] },
         { provide: NewDrawingService, useValue: newDrawingService },
         { provide: DrawingService, useValue: drawingService },
         { provide: ColorPickerService, useValue: colorPickerService }],
-    })
-      .compileComponents();
+    });
+    TestBed.compileComponents();
+    spyOn(TestBed.get(MatDialog), 'open').and.returnValue(dialogRefSpyObj);
   }));
 
   beforeEach(() => {
