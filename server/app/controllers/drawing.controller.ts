6bd19a630da6046ca0028f928eda7e14435a870f
Philippe Turner <philippeturner@gmail.com>
2019-10-05 12:10:11 -0400

    [done] - ajout de la mise en forme des requetes drawing

--- server/app/controllers/drawing.controller.ts
+++ server/app/controllers/drawing.controller.ts
@@ -22,10 +22,7 @@ export class DrawingController {
                     console.log(d);
                     res.json(d);
                 }).catch((reason: unknown) => {
-                    const errorMessage: Drawing = {
-                        name: `Error`,
-                    };
-                    res.json(errorMessage);
+                    res.json('error');
                 });
             });
 
@@ -36,25 +33,19 @@ export class DrawingController {
                     console.log(d);
                     res.json(d);
                 }).catch((reason: unknown) => {
-                    const errorMessage: Drawing = {
-                        name: `Error`,
-                    };
-                    res.json(errorMessage);
+                    res.json('error');
                 });
             });
 
         this.router.post('/',
             (req: Request, res: Response, next: NextFunction) => {
                 // Send the request to the service and send the response
-                const drawing: Drawing = { name: req.body.name };
+                const drawing: Drawing = { name: req.body.name, tags: req.body.tags, drawingObjects: req.body.drawingObjects };
                 this.drawingService.setDrawing(drawing).then((m: string) => {
                     console.log(m);
                     res.json(m);
                 }).catch((reason: unknown) => {
-                    const errorMessage: Drawing = {
-                        name: `Error`,
-                    };
-                    res.json(errorMessage);
+                    res.json('error');
                 });
             });
 
@@ -65,10 +56,7 @@ export class DrawingController {
                     console.log(m);
                     res.json(m);
                 }).catch((reason: unknown) => {
-                    const errorMessage: Drawing = {
-                        name: `Error`,
-                    };
-                    res.json(errorMessage);
+                    res.json('error');
                 });
             });
     }
