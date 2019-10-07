c91fa958dfb79c77dfe9e7c78a5d9aa3458d38b8
Sophie <s.dorval06@gmail.com>
2019-10-06 22:46:21 -0400

    Etampe fini avec test qui succeed et un coverage de 100%

--- client/src/app/objects/object-etampe/etampe.spec.ts
+++ client/src/app/objects/object-etampe/etampe.spec.ts
@@ -16,6 +16,16 @@ describe('EtampeObject', () => {
     const y = (object.y - (object.height / 2));
     expect(object.draw()).toEqual(
       '<image id="' + object.id + '" x="' + x + '" y="' + y +
-      '" width="' + object.width + '" height="' + object.height + '" xlink:href=' + object.url + ' />');
+      '" width="' + object.width + '" height="' + object.height + '" xlink:href=' + object.url +
+      '" transform=' + 'rotate(0,0,0)' + ' />');
   });
+
+  it('should return rotation string', () => {
+    const object: EtampeObject = new EtampeObject(0, 0, '');
+    object.angle = 10;
+
+    expect(object.getRotation()).toEqual(
+      '" transform=' + 'rotate(10,0,0)');
+  });
+
 });
--- client/src/test.ts
+++ client/src/test.ts
@@ -20,7 +20,7 @@ getTestBed().initTestEnvironment(
 );
 // Then we find all the tests.
 //const context = require.context('./', true, /.spec\.ts$/);
-const context = require.context('./', true, /etampe-tool-parameter.component.spec\.ts$/);
+const context = require.context('./', true, /etampe.spec\.ts$/);
 
 // And load the modules.
 context.keys().map(context);
