1a79dde14d8fbc5eb5fd4ab512671e182be7409c
Simon Zhang <babysheebo@gmail.com>
2019-10-11 17:22:58 -0400

    [wip] - ajouter l'algorithme de polygone et son modele

---
+++ client/src/app/objects/object-polygone/polygone.ts
@@ -0,0 +1,126 @@
+import { DrawingObject } from '../../../../../common/communication/drawing';
+import { RGBA } from '../../model/rgba.model';
+import { IObjects } from '../IObjects';
+
+/// Classe pour créer les objets ellipses
+export class PolygoneObject implements IObjects {
+  primaryColor: RGBA;
+  secondaryColor: RGBA;
+  id = 1;
+  x = 0;
+  y = 0;
+
+  height = 0;
+  width = 0;
+
+  vertexNumber = 0;
+  const initialAngle = 90;
+  _points: {x: number, y: number}[];
+
+  strokeWidth = 0;
+  style = '';
+
+  constructor(x: number, y: number, vertexNumber: number, strokeWidth: number, style: string) {
+    this.x = x;
+    this.y = y;
+    this.strokeWidth = strokeWidth;
+    this.style = style;
+  }
+
+  /// Pour definir le style du l'ellipse (complet, contour, centre)
+  getStyle(): string {
+    switch (this.style) {
+      case 'center': {
+        return '"fill:rgba('
+          + this.primaryColor.rgb.r + ',' + this.primaryColor.rgb.g + ',' + this.primaryColor.rgb.b + ',' + this.primaryColor.a + ')"';
+      }
+      case 'border': {
+        return '"fill:none;stroke-width:' + this.strokeWidth +
+          ';stroke:rgb(' +
+          this.secondaryColor.rgb.r +
+          ',' + this.secondaryColor.rgb.g + ',' + this.secondaryColor.rgb.b + ',' + this.secondaryColor.a + ')"';
+
+      }
+      case 'fill': {
+        return '"fill:rgb(' +
+          this.primaryColor.rgb.r + ',' + this.primaryColor.rgb.g + ',' + this.primaryColor.rgb.b +
+          ',' + this.primaryColor.a + ');stroke-width:' + this.strokeWidth + ';stroke:rgb('
+          + this.secondaryColor.rgb.r + ',' + this.secondaryColor.rgb.g + ',' + this.secondaryColor.rgb.b +
+          ',' + this.secondaryColor.a + ')"';
+      }
+      default: {
+        return '';
+      }
+    }
+  }
+
+  /// Pour retourner la ligne svg de l'ellipse pour le dessiner
+  draw(): string {
+    if (this.strokeWidth > 0) {
+      const points: string = this.getPointsString(this.points);
+      return '<polygon id="' + this.id
+      + 'points="' + points + '"'
+      + '" style=' + this.getStyle() + ' />';
+    }
+    return '';
+  }
+
+  get points(): {x: number, y: number}[] {
+    /// update the array
+    this.getPoints();
+    /// return the array
+    return this._points;
+  }
+
+  private getPoints() {
+    /// determine circle angles
+      const angle = 360 / this.vertexNumber;
+    /// determine radius
+      const radius = this.width / 2;
+    /// determine x and y from origin and initial angle
+      this.getPointsXandY(radius, 0);
+    /// repeat last step but add angle as you go for the n-1 remaining sides/points
+      let angleToAdd = 0;
+      for (let i = 1; i < this.vertexNumber; i++ ) {
+        angleToAdd += angle;
+        this.getPointsXandY(radius, angleToAdd);
+      }
+  }
+
+  getPointsXandY(radius: number, angleToAdd: number) {
+    const x = this.x + this.width / 2 + radius * Math.sin(this.getRAD((this.initialAngle + angleToAdd) % 360));
+    const y = this.y + this.height / 2 + radius * Math.cos(this.getRAD((this.initialAngle + angleToAdd) % 360));
+    this.points.push({x, y});
+  }
+
+  getRAD(angle: number) {
+    return angle * Math.PI / 180;
+  }
+
+  getPointsString(points: {x: number, y: number}[]) {
+    let tempString = '';
+    for (const point of points) {
+      tempString += point.x + ',' + point.y + ' ';
+    }
+    return tempString;
+  }
+
+  /// Retourne le drawing object pour un ellipse
+  toDrawingObject(): DrawingObject {
+    const drawingObject = {
+      type: 'polygone',
+      objectId: this.id,
+      x: this.x,
+      y: this.y,
+      height: this.height,
+      width: this.width,
+      primaryRGBA: this.primaryColor,
+      secondaryRGBA: this.secondaryColor,
+      pointsList: [],
+      strokeWidth: this.strokeWidth,
+      testureId: 0,
+      style: this.style,
+    };
+    return drawingObject;
+  }
+}
--- client/src/app/services/tools/polygon-tool/polygon-tool.service.ts
+++ client/src/app/services/tools/polygon-tool/polygon-tool.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faDrawPolygon, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 import { Point } from 'src/app/model/point.model';
-import { Polyline } from 'src/app/objects/object-polyline/polyline';
+import { PolygoneObject } from 'src/app/objects/object-polygone/polygone';
 import { ITools } from '../ITools';
 import { ToolIdConstants } from '../tool-id-constants';
+import { DrawingService } from '../../drawing/drawing.service';
+import { OffsetManagerService } from '../../offset-manager/offset-manager.service';
+import { ToolsColorService } from '../../tools-color/tools-color.service';
 
 @Injectable({
   providedIn: 'root',
@@ -13,21 +16,98 @@ export class PolygonService implements ITools {
 
   readonly toolName = 'Outil Polygon';
   readonly faIcon: IconDefinition = faDrawPolygon;
-  readonly id = ToolIdConstants.POLYGON_ID;
-  private object: Polyline | null;
-  private strokeWidth: FormControl;
-  private lastPoint: Point;
+  readonly id = ToolIdConstants.POLYGONE_ID;
+
+  private object: PolygoneObject | null;
+
   parameters: FormGroup;
+  private strokeWidth: FormControl;
+  private polygonStyle: FormControl;
+  private vertexNumber: FormControl;
+  private lastPoint: Point;
 
-  constructor() { }
+  oldX = 0;
+  oldY = 0;
+  firstX: number;
+  firstY: number;
 
+  constructor(private drawingService: DrawingService, private offsetManager: OffsetManagerService, private colorTool: ToolsColorService) {
+    this.strokeWidth = new FormControl(1, Validators.min(1));
+    this.polygonStyle = new FormControl('fill');
+    this.vertexNumber = new FormControl(3, Validators.min(3));
+    this.parameters = new FormGroup({
+      strokeWidth: this.strokeWidth,
+      rectStyle: this.polygonStyle,
+      vertexNumber: this.vertexNumber,
+    });
+  }
+
+  /// Quand le bouton de la souris est enfoncé, on crée un polygone regulier et convex.
+  /// On le retourne en sortie et il est inséré dans l'objet courant de l'outil.
   onPressed(event: MouseEvent): import("../../../objects/IObjects").IObjects | null {
-    throw new Error("Method not implemented.");
+    const offset: { x: number, y: number } = this.offsetManager.offsetFromMouseEvent(event);
+    this.firstX = offset.x;
+    this.firstY = offset.y;
+    this.object = new PolygoneObject(offset.x, offset.y, this.vertexNumber.value, this.strokeWidth.value , this.polygonStyle.value);
+    if (event.button === 0) {
+      this.object.primaryColor = { rgb: this.colorTool.primaryColor, a: this.colorTool.primaryAlpha };
+      this.object.secondaryColor = { rgb: this.colorTool.secondaryColor, a: this.colorTool.secondaryAlpha };
+    } else {
+      this.object.primaryColor = { rgb: this.colorTool.secondaryColor, a: this.colorTool.secondaryAlpha };
+      this.object.secondaryColor = { rgb: this.colorTool.primaryColor, a: this.colorTool.primaryAlpha };
+    }
+    return this.object;
   }
+
+  /// Quand le bouton de la sourie est relaché, il n'existe plus d'objet courrant de l'outil.
   onRelease(event: MouseEvent): void {
-    throw new Error("Method not implemented.");
+    this.object = null;
   }
+
+  /// Transforme le size de l'objet courant avec un x et un y en entrée
   onMove(event: MouseEvent): void {
-    throw new Error("Method not implemented.");
+    if (this.object) {
+      const offset: { x: number, y: number } = this.offsetManager.offsetFromMouseEvent(event);
+      this.setSize(offset.x, offset.y);
+    }
   }
+
+  private setSize(x: number, y: number): void {
+    if (this.object) {
+      this.oldX = x;
+      this.oldY = y;
+
+      this.object.width = x - this.firstX;
+      this.object.height = y - this.firstY;
+
+      if (this.object.width < 0) {
+        this.object.x = x;
+        this.object.width = this.firstX - this.object.x;
+      }
+      if (this.object.height < 0) {
+        this.object.y = y;
+        this.object.height = this.firstY - this.object.y;
+      }
+
+      if (y < this.firstY && x < this.firstX) {
+          if (this.object.width < this.object.height) {
+            this.object.height = this.object.width;
+            this.object.y = this.firstY - this.object.width;
+          } else {
+            this.object.width = this.object.height;
+            this.object.x = this.firstX - this.object.height;
+          }
+        } else if (this.object.width < this.object.height) {
+          this.object.height = this.object.width;
+          if (y < this.firstY) {
+            this.object.y = this.firstX + this.firstY - x;
+          }
+        } else {
+          this.object.width = this.object.height;
+          if (x < this.firstX) {
+            this.object.x = this.firstX + this.firstY - y;
+          }
+        }
+      }
+    }
 }
