cd31839da3b093be1fafacf1cfda7ceec7fbd254
Philippe Turner <philippeturner@gmail.com>
2019-10-18 11:11:29 -0400

    [wip] - Ajustement opacity pour color-applier

--- client/src/app/model/object-structure.model.ts
+++ client/src/app/model/object-structure.model.ts
@@ -1,7 +1,7 @@
 export const OBJECT_ATTRIBUTE_STRUCTURE: Record<string, Record<string, string>> = {
     polyline: {
         primaryColor: 'stroke',
-        primaryOpacity: 'stroke-Opacity',
+        primaryOpacity: 'stroke-opacity',
         secondaryColor: 'none',
         secondaryOpacity: 'none',
         x: 'x',
@@ -12,9 +12,9 @@ export const OBJECT_ATTRIBUTE_STRUCTURE: Record<string, Record<string, string>> 
 
     rect: {
         primaryColor: 'fill',
-        primaryOpacity: 'fill-Opacity',
+        primaryOpacity: 'fill-opacity',
         secondaryColor: 'stroke',
-        secondaryOpacity: 'stroke-Opacity',
+        secondaryOpacity: 'stroke-opacity',
         x: 'x',
         y: 'y',
         width: 'width',
@@ -23,9 +23,9 @@ export const OBJECT_ATTRIBUTE_STRUCTURE: Record<string, Record<string, string>> 
 
     ellipse: {
         primaryColor: 'fill',
-        primaryOpacity: 'fill-Opacity',
+        primaryOpacity: 'fill-opacity',
         secondaryColor: 'stroke',
-        secondaryOpacity: 'stroke-Opacity',
+        secondaryOpacity: 'stroke-opacity',
         x: 'cx',
         y: 'cy',
         width: 'width',
