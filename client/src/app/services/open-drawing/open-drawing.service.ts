e8c7c1163e072054a1556da0b49e10d6e5c06619
Kevin Nguyen <xuannamkevin@gmail.com>
2019-10-21 13:37:25 -0400

    [wip] refactoring de open drawing

--- client/src/app/services/open-drawing/open-drawing.service.ts
+++ client/src/app/services/open-drawing/open-drawing.service.ts
@@ -5,6 +5,7 @@ import { Drawing } from '../../../../../common/communication/drawing';
 import { catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { MatDialog } from '@angular/material';
+import { RGBA } from 'src/app/model/rgba.model';
 // import { IndexService } from '../index/index.service';
 
 @Injectable({
@@ -20,5 +21,23 @@ export class OpenDrawingService {
     );
 
   }
+  getBackground(drawing: Drawing): string {
+    const rgba: RGBA = drawing.backGroundColor;
+    return `rgba(${rgba.rgb.r},${rgba.rgb.g},${rgba.rgb.b},${rgba.a})`;
+  }
+  // getThumbnail(drawingObject: Drawing) {
+  //   const container: HTMLElement | null = document.getElementById(drawingObject.name);
+  //   if (container) {
+  //     const svgThumbnail: Element | null = container.children.item(0);
+
+  //     if (svgThumbnail) {
+  //       this.renderer.setAttribute(svgThumbnail, 'viewBox', `0 0 ${drawingObject.width} ${drawingObject.height}`);
+
+  //       svgThumbnail.innerHTML = `${drawingObject.svg}`;
+  //     }
+  //   }
+  // }
+
+
 
 }
